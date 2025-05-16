c1764c2aeeda7978556eb65a4346b857e63da52f
haizhoux <105341277+Haizhoux@users.noreply.github.com>
2025-05-16 16:01:43 -0400

    Update test_student_solution.py

--- test_student_solution.py
+++ test_student_solution.py
@@ -1,7 +1,10 @@
-from student_solution import sum_even_numbers
+from student_solution import your_function
 
 def test_case_1():
-    assert sum_even_numbers([1, 2, 3, 4]) == 6
+    assert your_function(85) == 'B'
 
 def test_case_2():
-    assert sum_even_numbers([0, 7, 8]) == 8
+    assert your_function(100) == 'A'
+
+def test_case_3():
+    assert your_function(59) == 'F'
