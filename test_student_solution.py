31a453e8a1ec1727a8c809f960663d74f3450ca8
haizhoux <105341277+Haizhoux@users.noreply.github.com>
2025-05-16 15:14:48 -0400

    Update test_student_solution.py

--- test_student_solution.py
+++ test_student_solution.py
@@ -1,7 +1,7 @@
 from student_solution import sum_even_numbers
 
 def test_case_1():
-    assert sum_even_numbers() == None  # TODO: update test
+    assert sum_even_numbers([1, 2, 3, 4]) == 6
 
 def test_case_2():
-    assert True  # Add more tests
+    assert sum_even_numbers([0, 7, 8]) == 8
