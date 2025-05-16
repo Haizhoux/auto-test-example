03fd108dfc22651ab680a2f54b6ab15d5899e365
haizhoux <105341277+Haizhoux@users.noreply.github.com>
2025-05-16 16:07:35 -0400

    Update test_student_solution.py

--- test_student_solution.py
+++ test_student_solution.py
@@ -1,10 +1,13 @@
-from student_solution import your_function
+from student_solution import determine_number_category
 
 def test_case_1():
-    assert your_function(85) == 'B'
+    assert determine_number_category(42) == 'Positive (Small)'
 
 def test_case_2():
-    assert your_function(100) == 'A'
+    assert determine_number_category(100) == 'Positive (Large)'
 
 def test_case_3():
-    assert your_function(59) == 'F'
+    assert determine_number_category(0) == 'Zero'
+
+def test_case_4():
+    assert determine_number_category(-3) == 'Negative'
