b20a9a9bfe958887f1fe530dd76966866d758fdf
haizhoux <105341277+Haizhoux@users.noreply.github.com>
2025-05-16 16:07:52 -0400

    Update student_solution.py

--- student_solution.py
+++ student_solution.py
@@ -1,7 +1,3 @@
-def sum_even_numbers(lst):
-    return sum(x for x in lst if x % 2 == 0)
-
-# test trigger
-# test trigger2
-# test trigger3
-# test trigger4
+def determine_number_category(number):
+    # TODO: implement this
+    pass
